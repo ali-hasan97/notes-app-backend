fc095494206c4eec3eef9b47b7e3f6641701bc89
ali <ali_hasan97@hotmail.com>
2022-09-27 03:54:48 -0500

    partially connected backend to database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // imports Node's built-in web server module
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
@@ -7,6 +8,9 @@ const app = express()
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
+
+// model Note created based on the schema of noteSchema
+const Note = require('./models/note')
 
 let notes = [
     {
@@ -44,17 +48,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -75,22 +77,21 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
-    
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId()
-    }
-    
-    notes = notes.concat(note)
-    response.json(note)
-})
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
@@ -98,7 +99,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
